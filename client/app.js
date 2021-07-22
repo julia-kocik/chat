a4c82e70a78993cabe27b0e426dfcfc990ffe2ee
julia-kocik <74269572+julia-kocik@users.noreply.github.com>
2021-07-22 14:56:15 +0200

    Add client

---
+++ client/app.js
@@ -0,0 +1,45 @@
+const loginForm = document.getElementById('welcome-form');
+const messagesSection = document.getElementById('messages-section');
+const messagesList = document.getElementById('messages-list');
+const addMessageForm = document.getElementById('add-messages-form');
+const userNameInput = document.getElementById('username');
+const messageContentInput = document.getElementById('message-content');
+let userName;
+
+const login = (e) => {
+    e.preventDefault();
+    if(userNameInput.value.length < 1) {
+        alert('error')
+    } else {
+        userName = userNameInput;
+        loginForm.classList.remove('show');
+        messagesSection.classList.add('show');
+    }
+}
+
+const addMessage = (author, content) => {
+    const message = document.createElement('li');
+    message.classList.add('message');
+    message.classList.add('message--received');
+    if(author === userName) {
+        message.classList.add('message--self');
+    }
+    message.innerHTML = `
+    <h3 class="message__author">${userName === author ? "You": author}</h3>
+    <div class="message__content">${content}</div>
+    `;
+    messagesList.appendChild(message);
+}
+
+const sendMessage = (e) => {
+    e.preventDefault();
+    if(messageContentInput.value.length < 1) {
+        alert('You have to type a message!')
+    } else {
+        addMessage(userName, messageContentInput.value);
+        messageContentInput.value = '';
+    }
+}
+
+loginForm.addEventListener('submit', login);
+addMessageForm.addEventListener('submit', sendMessage);
